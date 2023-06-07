58faf7bf850818da894bf4396aeb803b1705cfae
kalix.dev <50088673+kalix-dev@users.noreply.github.com>
2023-06-07 05:58:07 -0700

    Update firebase.config.js

--- scripts/firebase.config.js
+++ scripts/firebase.config.js
@@ -1,10 +1,11 @@
   // Initialize Firebase
   var config = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_DOMAIN",
-    databaseURL: "DATABASE_URL",
-    projectId: "PROJECT_ID",
-    storageBucket: "STORAGE_BUCKET",
-    messagingSenderId: "MESSAGING_SENDER_ID"
+  apiKey: "AIzaSyA9aeIFnYrQ9UXIUJS0RsPUbgljfinQdQ0",
+  authDomain: "a2za2za2z1.firebaseapp.com",
+  databaseURL: "https://a2za2za2z1-default-rtdb.firebaseio.com",
+  projectId: "a2za2za2z1",
+  storageBucket: "a2za2za2z1.appspot.com",
+  messagingSenderId: "1092163255405",
+  appId: "1:1092163255405:web:702ceabc4045e853ce5364"
   };
   firebase.initializeApp(config);
